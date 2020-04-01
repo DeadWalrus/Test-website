b5f4d3a781ffc5e8da38e6d1fd975caa8982f1ec
DeadWalrus <pointlesstack@gmail.com>
2020-04-01 18:10:01 -0400

    added some test javascript

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+const heading = document.querySelector('h1');
+heading.textContent = 'SHEEEIT';
+let cc = 0;
+document.querySelector('html').onclick = function()
+{
+    cc = cc + 1;
+    if(cc === 20)
+    {
+        alert('please stop clicking so much');
+        cc = 0;
+    }
+}
