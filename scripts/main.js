fc20f20b8636deb84bdc5c3cebc0eae8c09c43ee
DeadWalrus <pointlesstack@gmail.com>
2020-04-01 21:39:58 -0400

    added click counter to label

--- scripts/main.js
+++ scripts/main.js
@@ -4,9 +4,10 @@ let cc = 0;
 document.querySelector('html').onclick = function()
 {
     cc = cc + 1;
-    if(cc === 20)
+    /*if(cc === 20)
     {
         alert('please stop clicking so much');
         cc = 0;
-    }
+    }*/
+    document.querySelector('label').textContent = cc;
 }
